7f2c6c55f13ee1ab066dbafa3b80e9fbd1959b22
MbMyriam <myriam.mb@ynov.com>
2024-11-08 14:22:47 +0100

    Ajout enum messages

---
+++ src/lib/utils/messages.utils.ts
@@ -0,0 +1,7 @@
+export enum Messeys {
+    NOM_APPLI = 'Beasty',
+    SLOGAN = 'Bienvenue dans beasty ! Un endroit où nos animaux sont notre meilleure aide pour faire des rencontres !',
+    VALEUR_1 = 'Une safe place pour tout ceux qui aiment les animaux ',
+    VALEUR_2 = 'Des rencontres avec d’autres passionnés',
+    VALEUR_3 = 'Je n’ai plus d’idée mais c’est une maquette'
+}
