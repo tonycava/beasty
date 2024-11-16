23f3ec7fa823340079c51c9647d60f0260773672
Sarouman54 <sarahbarrabe@gmail.com>
2024-11-16 17:08:27 +0100

    add: personal profile block

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,57 @@
+<div class="h-screen">
+    <div class="flex h-1/2">
+        <div class="w-1/3 flex justify-center items-center">
+            <img src="/profile_picture.png" class="h-64 w-48 mt-6 rounded-3xl shadow-[-24px_-24px_#FFDB78]" alt="Photo de profil">
+        </div>
+        <div class="w-1/3 flex flex-col justify-center">
+            <div class="flex w-full">
+                <label class="w-1/3 text-right text-secondary" for="first_name">Prénom :</label>
+                <p class="w-3/4 ml-2" id="first_name">Jane</p>
+            </div>
+            <div class="flex w-full">
+                <label class="w-1/3 text-right text-secondary" for="last_name">Nom :</label>
+                <p class="w-3/4 ml-2" id="last_name">Doe</p>
+            </div>
+            <div class="flex w-full">
+                <label class="w-1/3 text-right text-secondary" for="age">Âge :</label>
+                <p class="w-3/4 ml-2" id="age">24 ans</p>
+            </div>
+            <div class="flex w-full">
+                <label class="w-1/3 text-right text-secondary" for="birthday">Anniversaire :</label>
+                <p class="w-3/4 ml-2" id="birthday">5 mai</p>
+            </div>
+            <div class="flex w-full">
+                <label class="w-1/3 text-right text-secondary" for="email">Adresse e-mail :</label>
+                <p class="w-3/4 ml-2" id="email">jane.doe@gmail.com</p>
+            </div>
+            <div class="flex w-full">
+                <label class="w-1/3 text-right text-secondary" for="bio">Bio :</label>
+                <p class="w-3/4 ml-2 text-justify" id="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <!-- <ul class="w-1/3 text-right text-secondary">
+                <li class="">Prénom :</li>
+                <li>Nom :</li>
+                <li>Âge :</li>
+                <li>Anniversaire :</li>
+                <li>Adresse e-mail :</li>
+                <li>Bio :</li>
+            </ul>
+            <ul class="w-2/3 ml-2">
+                <li class="">Jane</li>
+                <li>Doe</li>
+                <li>24 ans</li>
+                <li>5 mai</li>
+                <li>jane.doe@gmail.com</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
+            </ul> -->
+        </div>
+        <div class="w-1/3 flex flex-col justify-center items-center">
+            <button class="bg-accent text-white rounded-full w-3/5 mb-2">Modifier</button>
+            <button class="bg-accent text-white rounded-full w-3/5 mb-2">Ajouter un animal</button>
+            <button class="bg-white text-[#E91414] border border-secondary rounded-full w-3/5 mb-2">Supprimer le compte</button>
+        </div>
+    </div>
+    <div class="flex bg-green-500 h-1/2">
+        <p>lpl</p>
+    </div>
+</div>
