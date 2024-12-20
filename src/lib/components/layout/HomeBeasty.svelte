b94961e2266e3462c7cd4a4471a59f312e2fcc03
MbMyriam <myriam.mb@ynov.com>
2024-12-20 11:15:57 +0100

    Création section beasty

---
+++ src/lib/components/layout/HomeBeasty.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Messeys } from "$lib/utils/messages.utils";
+</script>
+
+<div class="w-full bg-secondary bg-opacity-10 h-72">
+    <h1 class="text-center font-normal text-6xl leading-tight py-6 mx-auto">
+        {Messeys.NOM_APPLI}
+    </h1>
+    <p class="text-center w-5/6 mx-auto ">
+        {Messeys.BEASTY_PREZ}
+    </p>
+</div>
