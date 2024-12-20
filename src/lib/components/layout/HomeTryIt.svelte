b5bc30c7080e16d92bb14237f7f783b44bf279d3
MbMyriam <myriam.mb@ynov.com>
2024-12-20 15:48:06 +0100

    ajout des sections

--- src/lib/components/layout/HomeTryIt.svelte
+++ src/lib/components/layout/HomeTryIt.svelte
@@ -1,3 +1,30 @@
 <script>
 	import { Messeys } from "$lib/utils/messages.utils";
 </script>
+<div class="max-w-6xl mx-auto">
+    <h1 class="font-bold text-2xl pt-10 leading-tight mb-6 ">
+        Fonctionnement
+    </h1>
+    <div class="flex justify-between">
+       <div>
+        <div class="bg-accent rounded-full">1</div>
+        <h2 class="font-bold">Créer un compte</h2>
+        <p>Connectez-vous grâce à votre compte google  
+            <button>en cliquant ici.</button>
+        </p>
+       </div>
+
+       <div>2
+        <h2 class="font-bold">Matchez avec des inconnus</h2>
+        <p>Rencontrer d’autres célibataires passionnés d’animaux 
+            <button>en cliquant ici.</button>
+        </p>
+       </div>
+       <div>3
+        <h2 class="font-bold">Discuter avec eux</h2>
+        <p>Discutez avec les autres utilisateurs lorsque vous aurez matché ensemble
+            <button>en cliquant ici.</button>
+        </p>
+       </div>
+    </div>
+</div>
