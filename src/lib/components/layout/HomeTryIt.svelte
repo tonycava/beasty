8a87fc27856665cef2a971f394056e7cfc6f57d1
MbMyriam <myriam.mb@ynov.com>
2024-12-20 16:29:01 +0100

    maj

--- src/lib/components/layout/HomeTryIt.svelte
+++ src/lib/components/layout/HomeTryIt.svelte
@@ -1,30 +1,36 @@
 <script>
 	import { Messeys } from "$lib/utils/messages.utils";
 </script>
-<div class="max-w-6xl mx-auto">
+<div class="max-w-6xl px-4 mx-auto">
     <h1 class="font-bold text-2xl pt-10 leading-tight mb-6 ">
         Fonctionnement
     </h1>
     <div class="flex justify-between">
        <div>
-        <div class="bg-accent rounded-full">1</div>
+        <div class="bg-accent rounded-full w-fit px-2">1</div>
         <h2 class="font-bold">Créer un compte</h2>
         <p>Connectez-vous grâce à votre compte google  
             <button>en cliquant ici.</button>
         </p>
        </div>
 
-       <div>2
+       <div>
+        <div class="bg-accent rounded-full w-fit px-2">2</div>
         <h2 class="font-bold">Matchez avec des inconnus</h2>
         <p>Rencontrer d’autres célibataires passionnés d’animaux 
             <button>en cliquant ici.</button>
         </p>
        </div>
-       <div>3
+       <div>
+        <div class="bg-accent rounded-full w-fit px-2">3</div>
         <h2 class="font-bold">Discuter avec eux</h2>
         <p>Discutez avec les autres utilisateurs lorsque vous aurez matché ensemble
             <button>en cliquant ici.</button>
         </p>
        </div>
     </div>
+</div>
+
+<div>
+    <img src="/home-chat.png" alt="image de chien" />
 </div>
