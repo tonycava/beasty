b7f85b74bd2c4a9dd1ef619e3af261ee790caab6
theoMANEVIT31 <theo.manevit@ynov.com>
2025-02-14 10:12:28 +0100

    fix

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -6,26 +6,26 @@ import { matcherRouter } from '../../modules/matcher/routes/MatcherRoute';
 import { AuthRouter } from '$auth/routes/AuthRouter';
 
 export const router = t.router({
-	matcherRouter,
-	getUser: t.procedure
-	.input(
-		z.string().uuid()
-	)
-	.query(async ({input}) => {
-		const getUserInfoUseCase = await GetUserInfoUseCase({
-			userRepository: SQLiteUserProfileRepository()
-		})
-		.execute(input);
+    matcherRouter,
+    getUser: t.procedure
+    .input(
+        z.string().uuid()
+    )
+    .query(async ({input}) => {
+        const getUserInfoUseCase = await GetUserInfoUseCase({
+            userRepository: SQLiteUserProfileRepository()
+        })
+        .execute(input);
 
-		if (getUserInfoUseCase.isSuccess) {
-			return getUserInfoUseCase.data
-		}
-		return null;
-	})
-	greeting: t.procedure.query(async () => {
-		return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()}`;
-	}),
-	authRouter: AuthRouter
+        if (getUserInfoUseCase.isSuccess) {
+            return getUserInfoUseCase.data
+        }
+        return null;
+    }), // Ajout de la virgule ici
+    greeting: t.procedure.query(async () => {
+        return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()}`;
+    }),
+    authRouter: AuthRouter
 });
 
 export type Router = typeof router;
